d09811cc956dddb75d6b7cf699f906a83e31e4e0
Blackblur91 <feketebalazs04@gmail.com>
2023-11-29 11:06:09 +0100

    added file reading

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 const app = express()
 const port = 3000
 
@@ -9,38 +10,25 @@ app.get('/', (req, res) => {
 
 app.use('/public', express.static(path.join(__dirname, '/frontend/public')))
 
-/* app.get('/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/public/style.css'))
+app.get('/beers', (req, res) => {
+  res.sendFile(path.join(__dirname, '/data/beers.json'))
 })
-
-app.get('/script.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/public/script.js'))
-}) */
-
-app.get('/kiskutya', (req, res) => {
-  //res.send('kiskutya!')
-  res.json("kiskutya")
-})
-
-app.get('/kismacska', (req, res) => {
-  //res.send('kismacska!')
-  res.json("kismacska")
-})
-
-/* app.get('/beers/1', (req, res) => {
-  res.json(`sending id: 1`)
-})
-
-app.get('/beers/2', (req, res) => {
-  res.json(`sending id: 2`)
-}) */
 
 app.get('/beers/:id', (req, res) => {
-  console.log(req.params.id)
   const id = parseInt(req.params.id)
   console.log(id)
 
-  res.json(`sending id: ${id}`)
+  fs.readFile(path.join(__dirname, '/data/beers.json'), 'utf8', (err, rawData) => {
+    if (err) {
+      console.log(err)
+      res.status(500).json(err)
+    } else {
+      const data = JSON.parse(rawData)
+      const foundBeer = data.find((beer) => beer.id === id)
+      if (foundBeer) res.json(foundBeer)
+      else res.status(404).json("id not exists")
+    }
+  })
 })
 
 app.listen(port, () => {
--- script.js
+++ script.js
@@ -2,13 +2,22 @@ console.log('loaded')
 const rootElement = document.querySelector("#root")
 
 const fetchUrl = async (url) => {
-  const data = await fetch (url)
-  return data.json()
+  const data = await fetch(url)
+  console.log(data)
+  if (data.status !== 200) {
+    return "bad request"
+  } else {
+    return data.json()
+  }
 }
 
 async function init() {
-  const data = await fetchUrl("/kismacska")
-  console.log(data)
+  const beerData = await fetchUrl("/beers/5")
+  if (beerData === "bad request") {
+    rootElement.innerHTML = "BAD USER"
+  } else {
+    rootElement.innerHTML = JSON.stringify(beerData)
+  }
 }
 
 init()
